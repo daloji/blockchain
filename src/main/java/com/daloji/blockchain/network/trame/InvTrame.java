83b2376533b00978dd0a39216af00f155aa57912
daloji <jiantila@gmail.com>
2020-01-22 17:38:09 +0100

    inventory message

--- src/main/java/com/daloji/blockchain/network/trame/InvTrame.java
+++ src/main/java/com/daloji/blockchain/network/trame/InvTrame.java
@@ -83,8 +83,6 @@ public class InvTrame extends TrameHeader{
 		System.arraycopy(msg, offset, buffer, 0, buffer.length);
 		this.setChecksum(Utils.bytesToHex(buffer));
 		offset = offset + buffer.length;
-		buffer = new byte[(int)length];
-		System.arraycopy(msg, offset, buffer, 0, buffer.length);
 		if(length <msg.length) {
 			byte[] info = new byte[4];
 			System.arraycopy(msg, offset, info, 0, info.length);
@@ -116,51 +114,56 @@ public class InvTrame extends TrameHeader{
 
 				for(int i=0;i<size;i++) {
 					buffer = new byte[4];
-					System.arraycopy(msg, offset, buffer, 0,buffer.length);
-					if(isValidType(buffer)) {
-						String type = Utils.bytesToHex(buffer);
-						offset = offset + buffer.length;
-						int decimalType = Integer.parseInt(Utils.StrLittleEndian(type),16);
-						buffer = new byte[32];
+					if((offset+buffer.length)<msg.length) {
 						System.arraycopy(msg, offset, buffer, 0,buffer.length);
-						offset = offset + buffer.length;
-						String hash = Utils.bytesToHex(buffer);
-						Inventory inventory = new Inventory();
-						switch (decimalType) {
-						case 0:
-							inventory.setType(InvType.ERROR);
-							inventory.setHash(hash);
-							listinv.add(inventory);
-							break;
-						case 1: 
-							inventory.setType(InvType.MSG_TX);
-							inventory.setHash(hash);
-							listinv.add(inventory);
-							break;
-						case 2: 
-							logger.info("ICIICICCCCCCCCCCCIICIICICI");
-							inventory.setType( InvType.MSG_BLOCK);
-							inventory.setHash(hash);
-							listinv.add(inventory);
-							break;
-						case 3: 
-							inventory.setType(InvType.MSG_FILTERED_BLOCK);
-							inventory.setHash(hash);
-							listinv.add(inventory);
-							break;
-						case 4: 
-							inventory.setType(InvType.MSG_CMPCT_BLOCK);
-							inventory.setHash(hash);
-							listinv.add(inventory);
-							break;
+						if(isValidType(buffer)) {
+							String type = Utils.bytesToHex(buffer);
+							offset = offset + buffer.length;
+							int decimalType = Integer.parseInt(Utils.StrLittleEndian(type),16);
+							buffer = new byte[32];
+							if((offset+buffer.length)<msg.length) {
+								System.arraycopy(msg, offset, buffer, 0,buffer.length);
+								offset = offset + buffer.length;
+								String hash = Utils.bytesToHex(buffer);
+								Inventory inventory = new Inventory();
+								switch (decimalType) {
+								case 0:
+									inventory.setType(InvType.ERROR);
+									inventory.setHash(hash);
+									listinv.add(inventory);
+									break;
+								case 1: 
+									inventory.setType(InvType.MSG_TX);
+									inventory.setHash(hash);
+									listinv.add(inventory);
+									break;
+								case 2: 
+									logger.info("ICIICICCCCCCCCCCCIICIICICI");
+									inventory.setType( InvType.MSG_BLOCK);
+									inventory.setHash(hash);
+									listinv.add(inventory);
+									break;
+								case 3: 
+									inventory.setType(InvType.MSG_FILTERED_BLOCK);
+									inventory.setHash(hash);
+									listinv.add(inventory);
+									break;
+								case 4: 
+									inventory.setType(InvType.MSG_CMPCT_BLOCK);
+									inventory.setHash(hash);
+									listinv.add(inventory);
+									break;
 
-						default:
-							break;
+								default:
+									break;
+								}	
+							}
+
+						}else {
+							offset = offset + buffer.length;
 						}
-					}else {
-						offset = offset + buffer.length;
-					}
-				}	
+					}	
+				}
 			}
 
 
