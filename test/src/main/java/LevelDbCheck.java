56edf64c692956bf8edbe97c6fd9e248270283b1
daloji <jiantila@gmail.com>
2020-02-25 10:13:56 +0100

    utilitaire de test

---
+++ test/src/main/java/LevelDbCheck.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import com.daloji.blockchain.core.Block;
+import com.daloji.blockchain.core.commons.database.proxy.LevelDbProxy;
+
+
+/**
+ * Verification des blocks headers deja telechargé
+ * @author daloji
+ *
+ */
+public class LevelDbCheck {
+
+	public static void main(String[] args) throws IOException {
+
+
+		Block bloc = new Block();
+		String genesisHash = bloc.getHashGenesisBloc();
+		String previoushash = genesisHash;
+		String lasthash = LevelDbProxy.getInstance().getLastHash();
+		bloc = LevelDbProxy.getInstance().findBlock(lasthash);
+		while(bloc!=null) {
+			previoushash = bloc.getPrevBlockHash();
+			bloc = LevelDbProxy.getInstance().findBlock(previoushash);
+			if(bloc != null) {
+				System.out.println(bloc);
+			}
+		}
+	}
+
+}
